00c5fe274232dd51e905153fab913502e174ae94
Iskandar Herputra Wahidiyat <iskandarherputra@gmail.com>
2020-08-21 20:46:24 +0700

    Setup landing page

---
+++ VehicleService/Home/HomeNonLoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  HomeNonLoginViewController.swift
+//  VehicleService
+//
+//  Created by Iskandar Herputra Wahidiyat on 21/08/20.
+//  Copyright © 2020 Iskandar Herputra Wahidiyat. All rights reserved.
+//
+
+import UIKit
+
+class HomeNonLoginViewController: UIViewController {
+    @IBOutlet weak var vehicleServiceTitleLabel: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    //MARK: - SETUP
+    private func setupView() {
+        registerButton.layer.borderWidth = 1.0
+        registerButton.layer.borderColor = UIColor.black.cgColor
+    }
+}
