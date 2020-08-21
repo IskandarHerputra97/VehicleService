a3293a87040739f8a29ac1dc30f0be6f2b40f5f8
Iskandar Herputra Wahidiyat <iskandarherputra@gmail.com>
2020-08-21 20:05:28 +0700

    Initial Commit

---
+++ VehicleServiceTests/VehicleServiceTests.swift
@@ -0,0 +1,34 @@
+//
+//  VehicleServiceTests.swift
+//  VehicleServiceTests
+//
+//  Created by Iskandar Herputra Wahidiyat on 21/08/20.
+//  Copyright © 2020 Iskandar Herputra Wahidiyat. All rights reserved.
+//
+
+import XCTest
+@testable import VehicleService
+
+class VehicleServiceTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
